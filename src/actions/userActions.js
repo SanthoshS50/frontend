b16a2e2101637652ad96dec98ffc62b7c42dbb39
Santhosh Srinivasan <santhoshsrinivasan29092001@gmail.com>
2024-03-12 11:44:43 +0530

    userActions Updated

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -49,6 +49,7 @@ export const login = (email, password) => async (dispatch) => {
             dispatch(loginRequest())
             const { data }  = await axios.post(`https://backend-2nri.onrender.com/api/v1/login`,{email,password});
             console.log(data);
+            localStorage.setItem('token',data.token)
             dispatch(loginSuccess(data))
         } catch (error) {
             dispatch(loginFail(error.response.data.message))
